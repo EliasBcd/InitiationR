bf5d3e7cb4ef0f3194e3a2f1e4c6cafab7d5b7c7
Elias Bouacida <elias@bouacida.fr>
2022-09-20 16:31:26 +0200

    Modifications des `CustomsTests.R` pour sourcer le même et créer le log avec le bon nom.

--- Assignations_et_objets/customTests.R
+++ Assignations_et_objets/customTests.R
@@ -11,48 +11,4 @@
 # can be used for for the purpose, but it also re-evaluates the
 # expression which the user entered, so care must be taken.
 
-# Get the swirl state
-getState <- function(){
-  # Whenever swirl is running, its callback is at the top of its call stack.
-  # Swirl's state, named e, is stored in the environment of the callback.
-  environment(sys.function(1))$e
-}
-
-# Retrieve the log from swirl's state
-getLog <- function(){
-  getState()$log
-}
-
-# Get the value which a user either entered directly or was computed
-# by the command he or she entered.
-getVal <- function(){
-  getState()$val
-}
-
-submit_log <- function(){
-  # Changer de lien et de nom pour chaque leçon
-  pre_fill_link <- "https://moodle.univ-paris8.fr/mod/assign/view.php?id=271762&action=editsubmission"
-  saved <- "Simples.csv"
-  temp <- tempfile()
-  
-  
-  p <- function(x, p, f, l = length(x)){if(l < p){x <- c(x, rep(f, p - l))};x}
-  
-  log_ <- getLog()
-  nrow_ <- max(unlist(lapply(log_, length)))
-  log_tbl <- data.frame(user = rep(log_$user, nrow_),
-                        course_name = rep(log_$course_name, nrow_),
-                        lesson_name = rep(log_$lesson_name, nrow_),
-                        question_number = p(log_$question_number, nrow_, NA),
-                        correct = p(log_$correct, nrow_, NA),
-                        attempt = p(log_$attempt, nrow_, NA),
-                        skipped = p(log_$skipped, nrow_, NA),
-                        datetime = p(log_$datetime, nrow_, NA),
-                        stringsAsFactors = FALSE)
-  write.csv(log_tbl, file = temp, row.names = FALSE)
-  encoded_log <- base64encode(temp)
-  write.csv(encoded_log, file=saved, row.names = FALSE)
-  if(getVal() == "Oui"){
-    browseURL(pre_fill_link)
-  }
-}
+source("customTests.R")
