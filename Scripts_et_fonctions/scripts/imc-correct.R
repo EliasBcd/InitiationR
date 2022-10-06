5145beef4538feb3ebc936ab9c1128caaa7dd099
Elias Bouacida <19273615+EliasBcd@users.noreply.github.com>
2022-10-06 20:23:56 +0200

    Premier test des scripts

---
+++ Scripts_et_fonctions/scripts/imc-correct.R
@@ -0,0 +1,4 @@
+tailles_corr <- c(156, 164, 197, 147, 173)
+poids_corr <- c(45, 59, 110, 44, 88)
+
+imc_corr <- poids / (tailles / 100) ^ 2
---
+++ Scripts_et_fonctions/scripts/imc.R
@@ -0,0 +1,10 @@
+# Dans ce script, vous devez assigner c(156, 164, 197, 147, 173) à tailles.
+# Ce sont des tailles en centimètres.
+#  c(45, 59, 110, 44, 88) à poids.
+# Et calculer l'IMC et assigner le résultat à imc.
+# Rappelez vous que l'imc vaut poids / taille^2, avec la taille en mètres.
+
+tailles <- c()
+poids <- c()
+
+imc <- 
