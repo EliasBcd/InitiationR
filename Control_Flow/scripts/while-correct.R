7f9778ee4d79e292a168a67e3c64aca317c25f50
Elias Bouacida <elias@bouacida.fr>
2022-10-25 17:45:51 +0200

    Début de l'écriture d'une question sur while.

---
+++ Control_Flow/scripts/while-correct.R
@@ -0,0 +1,24 @@
+# Nous vous donnons la fonction action() qui donne
+# l'évolution du prix d'une action au fil du temps.
+# Le prix d'origine de l'action est de 50€. 
+# Nous souhaitons la vendre dès que son prix dépasse 60€ pour empaucher les gains
+# ou descend sous les 40€ pour limiter les pertes. 
+# Ecrivez une fonction qui retourne le nombre de jours où l'on garde l'action.
+
+
+action <- function(x){
+  x + sample(c(-2, 2), 1)
+}
+
+nombre_jours <- function(){
+  jour <- 1
+  prix <- 50
+  
+  while (# condition){
+    # votre code
+  }
+  # code
+}
+
+
+
