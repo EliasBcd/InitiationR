a306be9962720efdcfa02a0b02c6ab34f7267548
Elias Bouacida <elias.bouacida@univ-paris8.fr>
2023-09-14 18:12:16 +0200

    Créations de la leçon fonctions et modifications de la leçon sur les scripts.

---
+++ Fonctions/customTests.R
@@ -0,0 +1,72 @@
+# Put custom tests in this file.
+      
+      # Uncommenting the following line of code will disable
+      # auto-detection of new variables and thus prevent swirl from
+      # executing every command twice, which can slow things down.
+      
+      # AUTO_DETECT_NEWVAR <- FALSE
+      
+      # However, this means that you should detect user-created
+      # variables when appropriate. The answer test, creates_new_var()
+      # can be used for for the purpose, but it also re-evaluates the
+      # expression which the user entered, so care must be taken.
+
+test_imc <- function(){
+  try({
+    t1 <- (tailles == c(156, 164, 197, 147, 173))
+    t2 <- (poids == c(45, 59, 110, 44, 88))
+    t3 <- (imc == c(45, 59, 110, 44, 88)/c(1.56, 1.64, 1.97, 1.47, 1.73)^2)
+    ok <- all(t1, t2, t3)   
+  }, 
+  silent=TRUE)
+  
+  # Returned as the result of the answer test.
+  exists('ok') && isTRUE(ok)
+}
+
+#### Notification to teacher.
+# Get the swirl state
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Retrieve the log from swirl's state
+getLog <- function(){
+  getState()$log
+}
+
+# Get the value which a user either entered directly or was computed
+# by the command he or she entered.
+getVal <- function(){
+  getState()$val
+}
+
+
+submit_log <- function(){
+  # Changer de lien et de nom pour chaque leçon
+  pre_fill_link <- "https://moodle.univ-paris8.fr/mod/assign/view.php?id=271762&action=editsubmission"
+  saved <- "Fonctions.txt"
+  temp <- tempfile()
+  
+  p <- function(x, p, f, l = length(x)){if(l < p){x <- c(x, rep(f, p - l))};x}
+  
+  log_ <- getLog()
+  nrow_ <- max(unlist(lapply(log_, length)))
+  log_tbl <- data.frame(user = rep(log_$user, nrow_),
+                        course_name = rep(log_$course_name, nrow_),
+                        lesson_name = rep(log_$lesson_name, nrow_),
+                        question_number = p(log_$question_number, nrow_, NA),
+                        correct = p(log_$correct, nrow_, NA),
+                        attempt = p(log_$attempt, nrow_, NA),
+                        skipped = p(log_$skipped, nrow_, NA),
+                        datetime = p(log_$datetime, nrow_, NA),
+                        stringsAsFactors = FALSE)
+  write.csv(log_tbl, file = temp, row.names = FALSE)
+  encoded_log <- base64enc::base64encode(temp)
+  write.csv(encoded_log, file=saved, row.names = FALSE)
+  if(getVal() == "Oui"){
+    browseURL(pre_fill_link)
+  }
+}
---
+++ Fonctions/scripts/imc.R
@@ -0,0 +1,20 @@
+# Dans ce script, vous devez assigner c(156, 164, 197, 147, 173) à tailles.
+# Ce sont des tailles en centimètres.
+#  c(45, 59, 110, 44, 88) à poids.
+# Et calculer l'IMC et assigner le résultat à imc.
+# Rappelez vous que l'imc vaut poids / taille^2, avec la taille en mètres.
+
+# Saisie des tailles et poids des enquêtés
+tailles <- c(156, 164, 197, 147, 173)
+poids <- c(45, 59, 110, 44, 88)
+
+# Calcul des tailles et poids moyens
+mean(tailles)
+mean(poids)
+
+# Calcul de l'IMC (poids en kilo divisé par les tailles en mètre au carré)
+imc <- poids / (tailles / 100) ^ 2
+
+# Valeurs extrêmes de l'IMC
+min(imc)
+max(imc)
