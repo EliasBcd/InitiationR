a7e2d7833d82b666eddf81fb7e2a87fca6ab7dd7
Elias Bouacida <elias.bouacida@univ-paris8.fr>
2023-09-14 19:21:49 +0200

    Ajouts des premiers exercices (incomplet).

---
+++ Exercice_1/initLesson.R
@@ -0,0 +1,15 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+swirl_options(swirl_logging = TRUE)
+
+conjoint1 <- c(1200, 1180, 1750, 2100)
+conjoint2 <- c(1450, 1870, 1690, 0)
+nb_personnes <- c(4, 2, 3, 2)
+
+conjoint_na <- c(1200, 1180, 1750, NA)
+
+temperatures <- c(3.4, 4.8, 8.4, 11.4, 15.8, 19.4, 22.2, 21.6, 17.6, 13.4, 7.6, 4.4)
+precipitations <- c(47.2, 44.1, 50.4, 74.9, 90.8, 75.6, 63.7, 62, 87.5, 98.6, 81.9, 55.2)
