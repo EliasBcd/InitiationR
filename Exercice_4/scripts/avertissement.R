9bf45ac187ad6b140d148bacdba8f9aae76ef4ac
Elias Bouacida <elias@bouacida.fr>
2023-11-08 12:23:10 +0100

    Enlevé la fonction message des instructions.

--- Exercice_4/scripts/avertissement.R
+++ Exercice_4/scripts/avertissement.R
@@ -1,8 +1,8 @@
 # Écrire une fonction avertissement qui prend deux arguments pluie et parapluie et qui effectue les opérations suivantes :
 #   
-# - si pluie vaut TRUE et parapluie vaut FALSE, renvoit “mouillé”
-# - si pluie vaut TRUE et parapluie vaut TRUE, renvoit “bien vu”
-# - si pluie vaut FALSE, renvoit “RAS”
+# - si pluie vaut TRUE et parapluie vaut FALSE, renvoie “mouillé”
+# - si pluie vaut TRUE et parapluie vaut TRUE, renvoie “bien vu”
+# - si pluie vaut FALSE, renvoie “RAS”
 #
 # Tapez submit() dans la console après avoir enregistré le script une fois que vous avez fini.
 
