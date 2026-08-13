fb1d7b02614efc3e3ff0110ee17eddf02aa4df7f
Elias Bouacida <elias.bouacida@univ-paris8.fr>
2026-08-13 15:13:19 +0200

    Mise à jour de la leçon Fonctions

--- Fonctions/scripts/imc.R
+++ Fonctions/scripts/imc.R
@@ -1,6 +1,6 @@
 # Dans ce script, vous devez assigner c(156, 164, 197, 147, 173) à tailles.
 # Ce sont des tailles en centimètres.
-#  c(45, 59, 110, 44, 88) à poids.
+# Et vous devez assigner c(45, 59, 110, 44, 88) à poids.
 # Et calculer l'IMC et assigner le résultat à imc.
 # Rappelez vous que l'imc vaut poids / tailles^2, avec la taille en mètres.
 
