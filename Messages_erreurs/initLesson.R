447983092472ee4c0bbaff472b59225ba584c9d6
Elias Bouacida <elias.bouacida@univ-paris8.fr>
2026-08-13 19:04:34 +0200

    Création d'une leçon sur les messages d'erreurs

---
+++ Messages_erreurs/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+swirl_options(swirl_logging = TRUE)
+
+tailles <- c(156, 164, 197, 147, 173)
+diplome <- c("CAP", "Bac", "Bac+2", "CAP", "Bac+3")
